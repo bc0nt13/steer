30878f0515abb852e093a6dfbae634dea1f7c8e4
Bruno Conti <53541286+bc0nt13@users.noreply.github.com>
2010-03-09 02:19:08 -0500

    Starting er-a: a wifi controlled vehicle with camera

---
+++ Classes/SteerAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SteerAppDelegate.h
+//  Steer
+//
+//  Created by Bruno Conti on 3/6/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+@interface SteerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *_window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
+
